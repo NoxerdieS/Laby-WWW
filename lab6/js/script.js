0e54fe785dbfdcbc9855b9631a8441a1f2911f56
Mateusz Bonat <mbonat42@gmail.com>
2025-04-06 12:52:26 +0200

    Added files

--- lab6/js/script.js
+++ lab6/js/script.js
@@ -1,31 +1,45 @@
 $(document).ready(function () {
-	// alert("Strona załadowana!");
+  alert("Strona załadowana!");
 
-	$('#header').delay(3000).slideUp();
+  $("#header").delay(3000).slideUp();
 
-	$('#showHideBtn').click(function () {
-		$('#listElement').toggle();
+  $("#showHideBtn").click(function () {
+    $("#listElement").toggle();
+  });
+
+  $("#text-color-btn").click(function () {
+    const selectedColor = $("#color-select").val();
+    $(".holiday-text").css("color", selectedColor);
+  });
+
+  $("#background-color-btn").click(function () {
+    const selectedColor = $("#color-select").val();
+    $(".holiday-text").css("background-color", selectedColor);
+  });
+
+  $("#apply-style-one").click(function () {
+    $("#greeting").removeClass("style-two").addClass("style-one");
+  });
+
+  $("#apply-style-two").click(function () {
+    $("#greeting").removeClass("style-one").addClass("style-two");
+  });
+
+  $("#text-input").on("change", function () {
+    const inputText = $(this).val();
+    $("#output-paragraph").text(inputText);
+  });
+
+  $("#thumbnail-container").on("click", "img", function() {
+	const newSrc = $(this).attr("src");
+	$("#main-image").attr("src", newSrc);
 	});
 
-	$(document).ready(function () {
-		$('#text-color-btn').click(function () {
-			const selectedColor = $('#color-select').val();
-			$('.holiday-text').css('color', selectedColor);
-		});
-
-		$('#background-color-btn').click(function () {
-			const selectedColor = $('#color-select').val();
-			$('.holiday-text').css('background-color', selectedColor);
-		});
-	});
-
-	$(document).ready(function () {
-		$('#apply-style-one').click(function () {
-			$('#greeting').removeClass('style-two').addClass('style-one');
-		});
-
-		$('#apply-style-two').click(function () {
-			$('#greeting').removeClass('style-one').addClass('style-two');
-		});
-	});
+	$("#add-task-btn").click(function() {
+        const task = $("#task-input").val().trim();
+        if (task !== "") {
+            $("#todo-list").append(`<li>${task}</li>`);
+            $("#task-input").val(""); 
+        }
+    });
 });
